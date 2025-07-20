73f75adc8fe343bbc974b5a3ee99508202caa33f
zac <punsalang.zachary@gmail.com>
2025-07-19 17:10:33 -0700

    chore: setup tsconfig to connect web to packages

--- packages/utils/src/index.tsx
+++ packages/utils/src/index.tsx
@@ -1,3 +1,5 @@
+export * from './supabase/middleware';
+export * from './supabase/server';
 export { toSlug } from './toSlug';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { usePrevious } from './usePrevious';
